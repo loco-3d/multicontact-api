4fbc20a77125d3f9624b6bf8ead742ea5a79c9d6
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-03-29 12:05:48 +0100

    [Python 3] fix import

--- bindings/python/multicontact_api/__init__.py
+++ bindings/python/multicontact_api/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2015-2018, CNRS
 # Authors: Justin Carpentier <jcarpent@laas.fr>
 
-from libmulticontact_api import *
+from multicontact_api.libmulticontact_api import *
 
 import numpy as np
