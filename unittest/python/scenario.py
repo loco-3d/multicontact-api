1bac00d788f7601e9991831d3874fdf1dbee94df
Pierre Fernbach <pierre.fernbach@laas.fr>
2019-12-06 14:39:46 +0100

    [Tests][Python] add unittest file for scenario module, test contact-model and contact-patch class

--- unittest/python/scenario.py
+++ unittest/python/scenario.py
@@ -3,22 +3,111 @@
 import unittest
 
 import numpy as np
-
+from random import uniform
+from math import sqrt,sin,cos
 import multicontact_api
-from multicontact_api import contact_patch
 multicontact_api.switchToNumpyArray()
+
+from multicontact_api import ContactModelPlanar,ContactPatch
+from pinocchio import SE3,Quaternion
+
+
+def randomQuaternion():
+  u1 = uniform(0.,1.)
+  u2 = uniform(0.,2.*np.pi)
+  u3 = uniform(0.,2.*np.pi)
+  a = sqrt(1-u1)
+  b = sqrt(u1)
+  q = Quaternion(a * sin(u2), a * cos(u2), b * sin(u3), b * cos(u3))
+  q.normalize()
+  return q
+
 
 class ContactModelTest(unittest.TestCase):
 
   def test_contact_model_planar(self):
-    #TODO
-    return
+    mu = 0.3
+    zmp_radius = 0.01
+    # constructor with both values
+    mp1 = ContactModelPlanar(mu,zmp_radius)
+    # test getter bindings
+    self.assertEqual(mp1.mu,mu)
+    self.assertEqual(mp1.ZMP_radius,zmp_radius)
+
+    # copy constructor :
+    mp2 = ContactModelPlanar(mp1)
+    self.assertEqual(mp2.mu,mu)
+    self.assertEqual(mp2.ZMP_radius,zmp_radius)
+
+    # test operator ==
+    self.assertTrue(mp1 == mp2)
+    mp1.mu = 0.5
+    self.assertTrue(mp1 != mp2)
 
 
 class ContactPatchTest(unittest.TestCase):
 
-  def test_contact_patch(self):
-    return
+  def test_default_constructor(self):
+    cp = ContactPatch()
+    self.assertEqual(cp.friction,-1.0)
+    self.assertTrue(cp.placement == SE3.Identity())
+
+  def test_setter_getter(self):
+    cp = ContactPatch()
+    p = SE3()
+    p.setRandom()
+    cp.placement = p
+    self.assertTrue(cp.placement == p)
+    cp.friction = 0.7
+    self.assertTrue(cp.friction == 0.7)
+    self.assertTrue(cp.placement == p)
+
+
+  def test_constructor_with_arguments(self):
+    p = SE3()
+    p.setRandom()
+    cp = ContactPatch(p)
+    self.assertTrue(cp.friction == -1.0)
+    self.assertTrue(cp.placement == p)
+    # check that the value have been copied and it's not the same pointer anymore :
+    p.setRandom()
+    self.assertTrue(cp.placement != p)
+
+    p = SE3()
+    p.setRandom()
+    cp = ContactPatch(p,0.9)
+    self.assertTrue(cp.friction == 0.9)
+    self.assertTrue(cp.placement == p)
+
+
+  def test_operator_equal(self):
+    cp1 = ContactPatch()
+    cp2 = ContactPatch()
+    self.assertTrue(cp1 == cp2)
+    self.assertFalse(cp1 != cp2)
+    cp1.friction = 0.5
+    self.assertTrue(cp1 != cp2)
+    self.assertFalse(cp1 == cp2)
+    cp2.friction = 0.5
+    self.assertTrue(cp1 == cp2)
+
+    p = SE3()
+    p.setRandom()
+    cp1 = ContactPatch(p,0.9)
+    cp2 = ContactPatch(p,0.9)
+    self.assertTrue(cp1 == cp2)
+    cp1.placement.setRandom()
+    self.assertTrue(cp1 != cp2)
+
+
+  def test_copy_constructor(self):
+    p = SE3()
+    p.setRandom()
+    cp1 = ContactPatch(p,0.9)
+    cp2 = ContactPatch(cp1)
+    self.assertTrue(cp1 == cp2)
+    cp1.placement.setRandom()
+    self.assertTrue(cp1 != cp2)
 
 
 
