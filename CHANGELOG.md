602bcfd85fa2bd8dde6040e129d0e57f4a23f924
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-14 18:29:07 +0200

    sync with gepetto/nix template

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+- Fix use of pinocchio 3 to get compatibility with pinocchio 4
 - CMake: drop git submodule
 - Nix: switch to flakoboros
 
--- flake.nix
+++ flake.nix
@@ -1,44 +1,27 @@
 {
   description = "API to define and store Contact phases and Contact Sequences.";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flakoboros.follows = "gepetto/flakoboros";
-    gazebros2nix.follows = "gepetto/gazebros2nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros = {
-              pyOverrideAttrs.multicontact-api = _: _: { };
-              overrideAttrs.multicontact-api = _: {
-                src = lib.fileset.toSource {
-                  root = ./.;
-                  fileset = lib.fileset.unions [
-                    ./bindings
-                    ./include
-                    ./notebooks
-                    ./unittest
-                    ./CMakeLists.txt
-                    ./package.xml
-                  ];
-                };
-              };
-            };
-          }
-        ];
+        extraDevPyPackages = [ "multicontact-api" ];
+        overrideAttrs.multicontact-api = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./bindings
+              ./include
+              ./notebooks
+              ./unittest
+              ./CMakeLists.txt
+              ./package.xml
+            ];
+          };
+        };
       }
     );
 }
